3105627647161812732d9b3e4874ebc0d4a430d5
Tumelo Mahlangu <tumelom@bbd.co.za>
2022-05-08 17:41:38 +0200

    updated calculator buttons

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,21 +1,29 @@
 
-const numericButtonSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const operationsButtonSymbols = ["DEL", "AC", "x", "/", "+", "-", "ANS", "="];
+const numericButtonSymbols = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
+const operationsButtonSymbols = ["DEL", "AC", "*", "/", "+", "-", "ANS", "="];
+const scientificSymbols = ["Abs", "Log", "sin", "cos", "tan", "(", ")", "ln"]
 const textField = document.getElementById("calculator__top-field");
 const numericButtonSection = document.getElementById("calculator__bottom-normal-button-numeric");
 const operationsButtonSection = document.getElementById("calculator__bottom-normal-button-operations");
 const answerSection = document.getElementById("calculator__top-answer");
-const NUMBER_OF_OPERATIONS_BUTTONS = 8;
+const scientificButtonSection = document.getElementById("calculator___bottom-scientific-button");
 
 
 for(let button=0; button<numericButtonSymbols.length; button++){
     const NormalbuttonElement = document.createElement("button");
     NormalbuttonElement.innerHTML = numericButtonSymbols[button];
-    NormalbuttonElement.classList.add("calculator-styled-buttons")
-    NormalbuttonElement.addEventListener("click" , ()=>{
+    NormalbuttonElement.classList.add("calculator-styled-buttons");
+    NormalbuttonElement.addEventListener("click",()=>{
         textField.innerHTML += numericButtonSymbols[button];
     })
     numericButtonSection.appendChild(NormalbuttonElement);
+}
+
+for(let button=0; button< scientificSymbols.length; button++){
+    const scientificButtonElement = document.createElement("button");
+    scientificButtonElement.innerHTML = scientificSymbols[button];
+    scientificButtonElement.classList.add("calculator-styled-buttons");
+    scientificButtonSection.appendChild(scientificButtonElement);
 }
 
 
@@ -23,23 +31,40 @@ for(let button=0; button<operationsButtonSymbols.length; button++){
     const operationButtonElement = document.createElement("button");
     operationButtonElement.innerHTML = operationsButtonSymbols[button];
     operationButtonElement.classList.add("calculator-styled-buttons")
-    operationButtonElement.addEventListener("click", ()=>{
-        if(operationsButtonSymbols[button] === "DEL")
+    operationButtonElement.addEventListener("click",()=>{
+        if(operationsButtonSymbols[button] === "DEL"){
             backSpace();
-        if(operationsButtonSymbols[button] === "=")
+        }
+        else if(operationsButtonSymbols[button] === "="){
             calculation();
-        else
+        }else if(operationsButtonSymbols[button] === "ANS"){
+            answerMemory();
+        }else if(operationsButtonSymbols[button] === "AC"){
+            resetMemory();
+        }
+        else{
             textField.innerHTML += operationsButtonSymbols[button]
-            
+        }
     })
     operationsButtonSection.appendChild(operationButtonElement);
 }
+
 
 const backSpace = () =>{
     textField.innerHTML = textField.innerHTML.substring(0, textField.innerHTML.length - 1);
 }
 
+const resetMemory = () => {
+    textField.innerHTML = "";
+    answerSection.innerHTML = "0";
+}
+
+const answerMemory = () =>{
+    let answerMemory = answerSection.innerHTML;
+    textField.innerHTML = answerMemory;
+    console.log(textField)
+}
+
 const calculation = () =>{
     answerSection.innerHTML = Function(`return(${textField.innerHTML})`)();
-    return answerSection;
 }
