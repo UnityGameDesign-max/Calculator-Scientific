0506904c40044755418d967dfdde0e962a606345
Tumelo Mahlangu <tumelom@bbd.co.za>
2022-05-12 04:33:08 +0200

    updated all functions

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,7 +1,7 @@
 const numericButtonSymbols = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0", "."];
 const operationsButtonSymbols = ["DEL", "AC", "x", "/", "+", "-", "ANS", "="];
 const scientificSymbols = [
-    "Abs", "Log", "sin", "cos", "tan", "(" , ")", 
+    "Abs", "Log", "sin", "cos", "tan", "rad" , "deg", 
     "ln", "%", "n!", "e", "x<sup>2</sup>",
     "x<sup>3</sup>", "x<sup>y</sup>", "π"
 ];
@@ -13,6 +13,27 @@ const answerSection = document.getElementById("calculator__top-answer");
 const scientificButtonSection = document.getElementById("calculator___bottom-scientific-button");
 const EULER_NUMBER = "2.71828182846";
 
+// const basicCalculation = (param1, param2, operation) => {
+//     let param1 = parseFloat(param1);
+//     let param2 = parseFloat(param2);
+//     let returnValue = 0;
+
+//     switch (operation){
+//         case "+":
+//             returnValue = param1 + param2;
+//         break;
+//         case "-":
+//             returnValue = param1 - param2;
+//         break;
+//         case "*":
+//             returnValue = param1 * param2;
+//         break;
+//         case "/":
+//             returnValue = param1 /param2;
+//         break;
+//     }
+//     return returnValue;
+// }
 
 for(let button=0; button<numericButtonSymbols.length; button++){
     const NormalbuttonElement = document.createElement("button");
@@ -24,11 +45,6 @@ for(let button=0; button<numericButtonSymbols.length; button++){
     numericButtonSection.appendChild(NormalbuttonElement);
 }
 
-const percentageConversion = (percentageStr) => {
-    let percentage = percentageStr.substring(0, percentageStr.length - 1);
-    return percentage * 1/100;
-}
-
 for(let button=0; button<scientificSymbols.length; button++){
     const scientificButtonElement = document.createElement("button");
     scientificButtonElement.innerHTML = scientificSymbols[button];
@@ -36,17 +52,32 @@ for(let button=0; button<scientificSymbols.length; button++){
     scientificButtonElement.addEventListener("click", ()=>{
         if(scientificSymbols[button] === "("){
             textField.innerHTML += scientificSymbols[button];
+        }else if(scientificSymbols[button] === ")"){
+            textField.innerHTML += scientificSymbols[button];
         }else if(scientificSymbols[button] === "%"){
-            textField.innerHTML += scientificSymbols[button];
+            percentageEvaluation();
         }else if(scientificSymbols[button] === "e"){
-            textField.innerHTML += scientificSymbols[button];
-            scientificButtonElement.setAttribute()
+           exponentialNumber();
         }else if(scientificSymbols[button] === "sin"){
-            sin();
+            sinEvaluation();
         }else if(scientificSymbols[button] === "cos"){
-            textField.innerHTML += scientificSymbols[button] + "("
+            cosEvaluation()
         }else if(scientificSymbols[button] === "Log"){
-            textField.innerHTML += scientificSymbols[button] + "(";
+            logEvaluation();
+        }else if(scientificSymbols[button] === "n!"){
+            factorialEvaluation(answerSection.innerHTML)
+        }else if(scientificSymbols[button] === "Abs"){
+            absoluteValue();
+        }else if(scientificSymbols[button] === "x<sup>2</sup>"){
+            squareNumberEvaluation();
+        }else if(scientificSymbols[button] === "x<sup>3</sup>"){
+            cubeNumberEvaluation();
+        }else if(scientificSymbols[button] === "tan"){
+            tanEvaluation();
+        }else if(scientificSymbols[button] === "x<sup>y</sup>"){
+            textField.innerHTML += "**"
+        }else if(scientificSymbols[button] === "rad"){
+            scientificButtonElement.classList.add("toggle-radians-degrees")
         }
     })
     scientificButtonSection.appendChild(scientificButtonElement);
@@ -67,8 +98,7 @@ for(let button=0; button<operationsButtonSymbols.length; button++){
             answerMemory();
         }else if(operationsButtonSymbols[button] === "AC"){
             resetMemory();
-        }
-        else{
+        }else{
             textField.innerHTML += operationsButtonSymbols[button];
         }
     })
@@ -80,39 +110,60 @@ const backSpace = () =>{
 }
 
 const equals = () => {
-    let indexOfUpperValue = textField.innerHTML.indexOf("^");
-    if(indexOfUpperValue > -1){
-        let base = (textField.innerHTML).slice(0, indexOfUpperValue);
-        let exponent = (textField.innerHTML).slice(0, indexOfUpperValue + 1)
-        answerSection.innerHTML = Function(`return(Math.pow(" + ${base} + "," + ${exponent}))`)();
-    }else{
-        answerSection.innerHTML += "";
-        answerSection.innerHTML = Function(`return(${textField.innerHTML})`)();                           
-    }
+    answerSection.innerHTML += "";
+    answerSection.innerHTML = Function(`return(${textField.innerHTML})`)();                       
 }
 
+const logEvaluation = () =>{
+    answerSection.innerHTML = Math.log(answerSection.innerHTML);
+}
+const factorialEvaluation = () =>{
+    console.log("factorial Evaluation!1")
+    if (answerSection.innerHTML < 0){
+        answerSection.innerHTML = -1
+        return answerSection.innerHTML;
+    }
+    else if (answerSection.innerHTML == 0){
+        answerSection.innerHTML= 1
+        return answerSection.innerHTML;
+    }
+    else {
+        answerSection.innerHTML = (answerSection.innerHTML * factorialEvaluation(answerSection.innerHTML - 1))
+        return answerSection.innerHTML;
+    }
+}
+const percentageEvaluation = () => {
+    answerSection.innerHTML = answerSection.innerHTML * 1/100;
+}
+
+const exponentialNumber = () => {
+    answerSection.innerHTML = EULER_NUMBER;
+}
+
+const absoluteValue = () => {
+    answerSection.innerHTML = Math.abs(answerSection.innerHTML);
+}
+const tanEvaluation = () => {
+    answerSection.innerHTML = Math.tan(answerSection.innerHTML);
+}
 const sinEvaluation = () =>{
-    textField.innerHTML = Math.sin(textField.innerHTML);
+    answerSection.innerHTML = Math.sin(answerSection.innerHTML);
+}
+const squareNumberEvaluation = () =>{
+    answerSection.innerHTML = Math.pow(answerSection.innerHTML,2);
+}
+
+const cubeNumberEvaluation = () => {
+    answerSection.innerHTML = Math.pow(answerSection.innerHTML,3);
 }
 const cosEvaluation = () => {
-    textField.innerHTML = Math.cos(textField.innerHTML);
+    answerSection.innerHTML = Math.cos(answerSection.innerHTML);
 }
 const resetMemory = () => {
     textField.innerHTML = "";
     answerSection.innerHTML = "";
 }
-
 const answerMemory = () =>{
     let answerMemory = answerSection.innerHTML;
     textField.innerHTML = answerMemory;
-}
-
-const calculation = () =>{
-    let answerField = "";
-    for(let i=0; i<textField.innerHTML.length; i++){
-        answerField += textField.innerHTML[i];
-    }
-    console.log(answerField);
-    console.log(percentageConversion(textField.innerHTML));
-    answerSection.innerHTML = Function(`return(${answerField})`)();
 }
--- src/CalculatorSymbols.js
+++
@@ -1,4 +0,0 @@
-const numericButtonSymbols = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const operationsButtonSymbols = ["DEL", "AC", "x", "/", "+", "-", "ANS", "="];
-
-module.exports ={numericButtonSymbols, operationsButtonSymbols}
